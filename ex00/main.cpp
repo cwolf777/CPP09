d9c6d8940d369f1d6c09c7fbb5444dd422ecc5ae
Christian Wolf <cwolf@2-E-1.42heilbronn.de>
2025-10-22 17:19:26 +0200

    delete comment in ex00 main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 09:57:30 by cwolf             #+#    #+#             */
-/*   Updated: 2025/10/07 10:16:20 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/10/22 17:15:03 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int main (int argc, char* argv[])
 {
-    // (void)argv[1];
     if (argc != 2)
     {
         std::cerr << "Wrong usage: ./btc <database>" << std::endl;
